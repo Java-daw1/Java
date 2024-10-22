0c56f8b8099cf4a2f8acb918d637cd23f21251b6
Zzelen29 <noxitowololo@gmail.com>
2024-10-22 18:43:26 +0200

    22/10/24

--- 01 _tipos_de_datos_estructuras_basicas/src/ejercicios/DecimalBinario.java
+++ 01 _tipos_de_datos_estructuras_basicas/src/ejercicios/DecimalBinario.java
@@ -8,7 +8,9 @@ public class DecimalBinario {
 
     public DecimalBinario(){
 
+//EJERCICIO 1 
 
+/* 
     Scanner leer =new Scanner(System.in);
 
     System.out.println("introduce un año");
@@ -22,21 +24,102 @@ public class DecimalBinario {
        }else{
         System.out.println("no es bisiesto");
        }
+ */
 
 
+
+ //EJERCICIO 2 SUMA PARES IMPARES Y RESTALOS
+
+ /* 
+int sumaPares = 0 ;
     
+System.out.println("********** PARES **********");
+for (int i = 2; i < 21; i+=2) {
+
+    sumaPares += i;
+
+    }
+
+System.out.println(sumaPares);
 
 
 
+int sumaImpar=0;
+int impar = 0;
+
+System.out.println("********** IMPARES **********");
+
+do {
+
+    impar++;
+
+    if (impar % 2 != 0 ) {
+
+        sumaImpar += impar ;
+         
+    }
+    
+} while (impar < 51);
+
+System.out.println(sumaImpar);
+
+System.out.println("********** SUMA Y RESTA DE LOS DOS **********");
+
+int suma3=0;
+int numero = 0;
 
 
 
+do {
+
+    numero++;
+
+    if (numero % 2 != 0 ) {
+
+        suma3 += numero ;
+         
+    }
+
+    if (numero % 2 == 0) {
+
+        suma3 -= numero;
+    }
+    
+} while (numero < 20);
+
+System.out.println(suma3);
+
+*/
+
+
+// EJERCICIO 5 BINARIO
+
+Scanner leer = new Scanner(System.in);
+
+
+ System.out.println("Introduzca un número entero para pasarlo a binario:");
+ int numleido = leer.nextInt();
+
+ String binario = "";
+
+ // si el numero introducido es cero no entra al bucle
+ if (numleido == 0) {
+
+     binario = "0";
+     
+ } else {
+     while (numleido > 0) {
+
+         binario = (numleido % 2) + binario; 
+         numleido /= 2; 
+     }
+ }
+
+ System.out.println("En binario es: " + binario);
 
 
 
-       
-
-
+ 
 
 
 
