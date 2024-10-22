35e86a5f0012ccf94dabd122b16067c69427bbc5
Zzelen29 <noxitowololo@gmail.com>
2024-10-22 23:55:45 +0200

    22/10/24 23:55

--- 01 _tipos_de_datos_estructuras_basicas/src/ejercicios/DecimalBinario.java
+++ 01 _tipos_de_datos_estructuras_basicas/src/ejercicios/DecimalBinario.java
@@ -26,8 +26,6 @@ public class DecimalBinario {
        }
  */
 
-
-
  //EJERCICIO 2 SUMA PARES IMPARES Y RESTALOS
 
  /* 
@@ -91,13 +89,79 @@ System.out.println(suma3);
 
 */
 
+//EJERCICIO 3
+
+/* 
+boolean initialized=false;
+int mayor=0,menor=0,entradaint=0;
+String mayor_name="",menor_name="", entrada=""; 
+Scanner sc = new Scanner(System.in);
+for(int i=1;i<=10;i++){
+    if (initialized==false){
+
+        System.out.println("Introduzca el nombre " + i);
+        entrada=sc.next();
+
+        System.out.println("Introduzca el sueldo neto mensual");
+        entradaint=sc.nextInt();
+
+        mayor=menor=entradaint;
+        mayor_name=menor_name=entrada;
+        initialized=true;
+    }
+    if (initialized==true&&i!=1){
+        System.out.println("Introduzca el nombre " + i);
+        entrada=sc.next();
+
+        System.out.println("Introduzca el sueldo neto mensual");
+        entradaint=sc.nextInt();
+
+        if(entradaint>mayor){
+            mayor=entradaint;
+            mayor_name=entrada;
+        }
+        if(entradaint<menor){
+            menor=entradaint;
+            menor_name=entrada;
+        }
+    }
+}
+
+sc.close();
+System.out.println("El mayor sueldo es de " + mayor + " y corresponde a " + mayor_name);
+System.out.println("El menor sueldo es de " + menor + " y corresponde a " + menor_name);
+*/
+
+//EJERCICIO 4 
+
+/* 
+int a=214, b=7, min=1;
+for (int i = 1; i <= a; i++) {
+    while (((a%i==0)||(b%i==0))&&i!=1) {
+        if ((a%i==0)&&(b%i==0)) {
+            min*=i;
+            a/=i;
+            b/=i;
+        } else if ((a%i!=0)&&(b%i==0)){
+            min*=i;
+            b/=i;
+        } else if ((a%i==0)&&(b%i!=0)){
+            min*=i;
+            a/=i;
+        }
+    }
+}
+System.out.println(min);
+*/
 
 // EJERCICIO 5 BINARIO
 
+/* 
 Scanner leer = new Scanner(System.in);
 
 
  System.out.println("Introduzca un número entero para pasarlo a binario:");
+
  int numleido = leer.nextInt();
 
  String binario = "";
@@ -108,6 +172,7 @@ Scanner leer = new Scanner(System.in);
      binario = "0";
      
  } else {
+
      while (numleido > 0) {
 
          binario = (numleido % 2) + binario; 
@@ -117,43 +182,7 @@ Scanner leer = new Scanner(System.in);
 
  System.out.println("En binario es: " + binario);
 
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 
 
     }
